79f940bed842b620d315ddacf53d374a3505a552
sysatom <sysatom@gmail.com>
2023-06-02 15:53:55 +0800

    async model example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,16 +4,80 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+use tokio::sync::{mpsc, Mutex};
+use tracing::info;
+
+struct AsyncProcInputTx {
+    inner: Mutex<mpsc::Sender<String>>
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn rs2js<R: tauri::Runtime>(message: String, manager: &impl Manager<R>) {
+    info!(?message, "rs2js");
+    manager
+        .emit_all("rs2js", format!("rs: {}", message))
+        .unwrap();
+}
+
+#[tauri::command]
+async fn js2rs(message: String, state: tauri::State<'_, AsyncProcInputTx>) -> Result<(), String> {
+    info!(?message, "js2rs");
+    let async_proc_input_tx = state.inner.lock().await;
+    async_proc_input_tx
+        .send(message)
+        .await
+        .map_err(|e| e.to_string())
+}
+
 fn main() {
+    tracing_subscriber::fmt::init();
+
+    let (async_proc_input_tx, async_proc_input_rx) = mpsc::channel(1);
+    let (async_proc_output_tx, mut async_proc_output_rx) = mpsc::channel(1);
+
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(AsyncProcInputTx {
+            inner: Mutex::new(async_proc_input_tx),
+        })
+        .invoke_handler(tauri::generate_handler![greet, js2rs])
+        .setup(|app| {
+            tauri::async_runtime::spawn(async move {
+                async_process_model(
+                    async_proc_input_rx,
+                    async_proc_output_tx,
+                ).await
+            });
+
+            let app_handle = app.handle();
+            tauri::async_runtime::spawn(async move {
+                loop {
+                    if let Some(output) = async_proc_output_rx.recv().await {
+                        rs2js(output, &app_handle);
+                    }
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn async_process_model(
+    mut input_rx: mpsc::Receiver<String>,
+    output_tx: mpsc::Sender<String>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    loop {
+        while let Some(input) = input_rx.recv().await {
+            let output = input;
+            output_tx.send(output).await?;
+        }
+    }
 }
--- src/app/bots/page.tsx
+++ src/app/bots/page.tsx
@@ -7,13 +7,29 @@ import {IBot} from "@/types";
 import fetcher from "@/helpers/http";
 import {OPERATE} from "@/constants/ACTION";
 import "./styles.css"
+import { listen } from '@tauri-apps/api/event';
+import { invoke } from '@tauri-apps/api/tauri';
 
 export default function BotsPage() {
   const [bots, setBots] = useState<IBot>(null)
 
+  const [output, setOutput] = useState("")
+
+  const sendOutput = () => {
+    invoke('js2rs', { message: output })
+  }
+
   useEffect(()=> {
     fetcher(OPERATE.bots).then(v => setBots(v)).catch(console.error)
+
+     listen('rs2js', (event) => {
+        console.log("rs2js: " + event.payload)
+      }).then(console.log).catch(console.error)
   }, [])
+
+  const handleChange = (event)=> {
+      setOutput(event.target.value)
+  }
 
   return (
     <div className="items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
@@ -22,6 +38,12 @@ export default function BotsPage() {
       </div>
       <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
         <BotInfo bots={bots} />
+      </div>
+      <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
+        <input type="text" value={output} onChange={handleChange} />
+          <button onClick={sendOutput}>
+            test
+          </button>
       </div>
     </div>
   )
