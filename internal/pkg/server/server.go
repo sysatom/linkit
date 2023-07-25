006af04f81d146e5bcd0e2c7e4eed7d4db553369
sysatom <sysatom@gmail.com>
2023-07-25 21:01:22 +0800

    use fiber middleware

--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -2,12 +2,18 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"net"
 )
 
 func EmbedServer(port string) {
 	go func() {
 		app := fiber.New()
+		app.Use(cors.New())
+		app.Use(recover.New())
+		app.Use(requestid.New())
 
 		app.Get("/", func(c *fiber.Ctx) error {
 			return c.SendString("ok")
